12f21d0f516bfd5476faed1bab135b03ba7eeb5b
Srihari Vavilapalli <7267644+svavilapalli@users.noreply.github.com>
2025-07-09 09:45:47 +0100

    Update maxfreetime.py

--- maxfreetime.py
+++ maxfreetime.py
@@ -49,6 +49,13 @@ class Solution:
         return res
 
 
+def maxFreeTime_faster(self, t: int, k: int, s: List[int], e: List[int]) -> int:
+        q = [*map(sub,s+[t],[0]+e)]
+        return max(accumulate(map(sub,q[k+1:],q),lambda p,w:p+w,initial=sum(q[:k+1])))
+        return max(accumulate(zip(q[k+1:],q),lambda p,w:p+w[0]-w[1],initial=sum(q[:k+1])))
+        return max(accumulate(range(k+1,len(q)),lambda p,i:p+q[i]-q[i-k-1],initial=sum(q[:k+1])))
+
+
 s= Solution()
 eventTime = 10
 k = 1
