92dfc435d23ef91bbbedec5a83dc912aede31404
Srihari Vavilapalli <7267644+svavilapalli@users.noreply.github.com>
2025-07-21 09:51:45 +0100

    Create gridchallengestrings.py

---
+++ gridchallengestrings.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    # Write your code here
+    n = len(grid)
+    if n <=1:
+        return "YES"
+    for i in range(n):
+        grid[i] = ''.join(sorted(grid[i]))
+        if i>0:
+            if grid[i] < grid[i-1]:
+                return "NO"
+    
+    return "YES"
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
